757e49dfe9a94b4eb8500bd9f9827ca2c55fcf14
ShayNeeo <phamquocthanh357@gmail.com>
2025-10-21 00:46:39 +0700

    fix

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
 use axum::Router;
+use tower::make::Shared;
 // removed unused import
 use dotenvy::dotenv;
 use std::{net::SocketAddr, sync::Arc};
@@ -42,7 +43,7 @@ async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     tracing::info!("listening on {}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, Shared::new(app)).await.unwrap();
 }
 
 
